e505a0b358b2669eb0f276a3eba882bc8cc349e8
Yuki <yuuki.walsh@outlook.com>
2023-08-21 23:32:02 +1000

    icons

---
+++ ScreenshotStudio/Controls/XivTexImage.cs
@@ -0,0 +1,55 @@
+﻿// © XivTools.
+// Licensed under the MIT license.
+
+namespace ScreenshotStudio.Controls;
+
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Lumina.Data.Files;
+using ScreenshotStudio.Plugin;
+using Serilog;
+
+public class XivTexImage : Image
+{
+	private static readonly Dictionary<string, CroppedBitmap> Cache = new();
+
+	public XivTexImage()
+	{
+		try
+		{
+			string cacheKey = $"{this.Path} ({this.Rect})";
+
+			if (Cache.TryGetValue(cacheKey, out CroppedBitmap? source))
+			{
+				this.Source = source;
+				return;
+			}
+
+			Log.Information($"Load xiv texture {cacheKey}");
+			TexFile? tex = DalamudServices.DataManager.GetFile<TexFile>(this.Path);
+
+			if (tex == null)
+				return;
+
+			BitmapSource bmp = BitmapSource.Create(tex.Header.Width, tex.Header.Height, 96, 96, PixelFormats.Bgra32, null, tex.ImageData, tex.Header.Width * 4);
+			bmp.Freeze();
+
+			Int32Rect rect = new((int)this.Rect.X, (int)this.Rect.Y, (int)this.Rect.Width, (int)this.Rect.Height);
+			CroppedBitmap newSource = new CroppedBitmap(bmp, rect);
+			this.Source = newSource;
+
+			Cache.Add(cacheKey, newSource);
+		}
+		catch (Exception ex)
+		{
+			Log.Error(ex, $"Error loading xiv texture {this.Path}");
+		}
+	}
+
+	public string Path { get; set; } = "ui/uld/icona_frame.tex";
+	public Rect Rect { get; set; } = new Rect(0, 0, 48, 48);
+}
---
+++ ScreenshotStudio/GameData/ImageReference.cs
@@ -0,0 +1,76 @@
+﻿// © XivTools.
+// Licensed under the MIT license.
+
+namespace ScreenshotStudio.GameData;
+
+using System;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Lumina.Data.Files;
+using ScreenshotStudio.Plugin;
+using Serilog;
+
+public class ImageReference
+{
+	public readonly uint ImageId;
+
+	private WeakReference<ImageSource>? cachedImage;
+
+	public ImageReference(uint imageId)
+	{
+		this.ImageId = imageId;
+	}
+
+	public ImageReference(ushort imageId)
+	{
+		this.ImageId = imageId;
+	}
+
+	public ImageReference(int imageId)
+	{
+		this.ImageId = (uint)imageId;
+	}
+
+	public ImageSource? Source
+	{
+		get
+		{
+			if (this.ImageId == 0)
+				return null;
+
+			ImageSource? img;
+			if (this.cachedImage != null && this.cachedImage.TryGetTarget(out img))
+			{
+				return img;
+			}
+
+			try
+			{
+				Log.Verbose($"Loading image {this.ImageId}");
+
+				////string path = $"ui/icon/{this.ImageId / 1000u * 1000:000000}/{this.ImageId:000000}.tex";
+				string path = $"ui/icon/{this.ImageId / 1000u * 1000:000000}/{this.ImageId:000000}_hr1.tex";
+				TexFile? tex = DalamudServices.DataManager.GetFile<TexFile>(path);
+
+				if (tex == null)
+					return null;
+
+				BitmapSource bmp = BitmapSource.Create(tex.Header.Width, tex.Header.Height, 96, 96, PixelFormats.Bgra32, null, tex.ImageData, tex.Header.Width * 4);
+				bmp.Freeze();
+				img = bmp;
+
+				if (this.cachedImage == null)
+					this.cachedImage = new WeakReference<ImageSource>(img);
+
+				this.cachedImage.SetTarget(img);
+				return img;
+			}
+			catch (Exception ex)
+			{
+				Log.Warning(ex, $"Failed to load Image: {this.ImageId} ");
+			}
+
+			return null;
+		}
+	}
+}
