02b0400925840f7d39ee754240e8eae4667f3fa2
Yuki <yuuki.walsh@outlook.com>
2023-08-30 16:55:41 +1000

    added DOL/DOH/DOW/DOM tags to classjob category tags

--- ScreenshotStudio/GameData/Excel/ClassJobCategory.cs
+++ ScreenshotStudio/GameData/Excel/ClassJobCategory.cs
@@ -6,11 +6,27 @@ namespace ScreenshotStudio.GameData.Excel;
 using Lumina.Data;
 using Lumina.Excel;
 using ScreenshotStudio.Tags;
+using System;
 
 [Sheet("ClassJobCategory", 2091841742u)]
 public class ClassJobCategory : LibraryExcelRow
 {
 	private readonly bool[] classJobs = new bool[(int)ClassJob.ClassJobRows.Count];
+
+	public enum ClassJobCategoryRows : uint
+	{
+		None,
+		AllClasses,
+
+		DiscipleOfWar = 30,
+		DiscipleOfMagic = 31,
+		DiscipleOfTheLand = 32,
+		DiscipleOfTheHand = 33,
+		DiscipleOfWarOrMagic = 34,
+		DiscipleOfTheLandOrHand = 45,
+		DiscipleOfWarOrMagic2 = 110,
+		DiscipleOfWarOrMagic3 = 192,
+	}
 
 	public string? Name { get; set; }
 
@@ -59,6 +75,41 @@ public class ClassJobCategory : LibraryExcelRow
 				}
 
 				tags.Add(classJob.ToTags());
+			}
+		}
+
+		// Special tags
+		if (Enum.IsDefined((ClassJobCategoryRows)this.RowId))
+		{
+			ClassJobCategoryRows row = (ClassJobCategoryRows)this.RowId;
+
+			if (this.RowId == (uint)ClassJobCategoryRows.DiscipleOfWar)
+			{
+				tags.Add(row.ToString()).WithAlias("DOW");
+			}
+			else if (this.RowId == (uint)ClassJobCategoryRows.DiscipleOfMagic)
+			{
+				tags.Add(row.ToString()).WithAlias("DOM");
+			}
+			else if (this.RowId == (uint)ClassJobCategoryRows.DiscipleOfTheLand)
+			{
+				tags.Add(row.ToString()).WithAlias("DOL");
+			}
+			else if (this.RowId == (uint)ClassJobCategoryRows.DiscipleOfTheHand)
+			{
+				tags.Add(row.ToString()).WithAlias("DOH");
+			}
+			else if (this.RowId == (uint)ClassJobCategoryRows.DiscipleOfWarOrMagic
+				|| this.RowId == (uint)ClassJobCategoryRows.DiscipleOfWarOrMagic2
+				|| this.RowId == (uint)ClassJobCategoryRows.DiscipleOfWarOrMagic3)
+			{
+				tags.Add(ClassJobCategoryRows.DiscipleOfWar.ToString()).WithAlias("DOW");
+				tags.Add(ClassJobCategoryRows.DiscipleOfMagic.ToString()).WithAlias("DOM");
+			}
+			else if (this.RowId == (uint)ClassJobCategoryRows.DiscipleOfTheLandOrHand)
+			{
+				tags.Add(ClassJobCategoryRows.DiscipleOfTheLand.ToString()).WithAlias("DOL");
+				tags.Add(ClassJobCategoryRows.DiscipleOfTheHand.ToString()).WithAlias("DOH");
 			}
 		}
 
