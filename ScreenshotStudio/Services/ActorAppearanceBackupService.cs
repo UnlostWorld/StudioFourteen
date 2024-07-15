1d16fdab08fd93ec15c0836bb64e7e8dda52b4a6
Yuki <yuuki.walsh@outlook.com>
2024-07-15 17:15:24 +1000

    group pose characters

--- ScreenshotStudio/Services/ActorAppearanceBackupService.cs
+++ ScreenshotStudio/Services/ActorAppearanceBackupService.cs
@@ -15,8 +15,8 @@ using WpfUtils;
 
 public class ActorAppearanceBackupService : ServiceBase
 {
-	private readonly CurrentActorsLibraryProvider provider = new();
-	private readonly Dictionary<ushort, Appearance> backup = new();
+	private readonly GroupPoseCharactersLibrarySource provider = new();
+	private readonly Dictionary<ushort, ActorBackupAppearance> backup = new();
 
 	public override Task Start()
 	{
@@ -86,95 +86,88 @@ public class ActorAppearanceBackupService : ServiceBase
 			this.provider.OnEnterGroupPose();
 		}
 	}
+}
 
-	public class Appearance : IActorAppearance
+public class ActorBackupAppearance : EntryBase, IActorAppearance
+{
+	private readonly string? name;
+
+	public unsafe ActorBackupAppearance(Actor* actor)
+		: base(null)
 	{
-		public unsafe Appearance(Actor* actor)
-		{
-			this.Name = actor->Name;
-			this.DrawData = actor->DrawData;
-			this.ModelId = actor->ModelCharaRowId;
+		this.name = actor->Name;
+		this.DrawData = actor->DrawData;
+		this.ModelId = actor->ModelCharaRowId;
 
-			this.Tags.Add("Named");
-		}
-
-		public Appearance(Actor actor)
-		{
-			this.Name = actor.Name;
-			this.DrawData = actor.DrawData;
-			this.ModelId = actor.ModelCharaRowId;
-
-			this.Tags.Add("Named");
-		}
-
-		public DrawDataContainer DrawData { get; private set; }
-		public uint ModelId { get; private set; }
-		public string? Name { get; private set; }
-		public TagCollection Tags { get; init; } = new();
-		public SourceBase? Source { get; set; }
-		public string Identifier => string.Empty;
-		public bool IsValid => true;
-
-		public unsafe void Apply(Actor* actor)
-		{
-			this.Apply(actor, Actor.UpdateSource.Library);
-		}
-
-		public unsafe void Apply(Actor* actor, Actor.UpdateSource source)
-		{
-			bool redraw = this.ModelId != actor->ModelCharaRowId;
-
-			actor->UpdateModel(this.ModelId, source, false);
-			actor->UpdateCustomize(this.DrawData.CustomizeData, redraw, source);
-			actor->UpdateEquipment(this.DrawData.EquipmentModelIds, source);
-		}
-
-		public void Dispose()
-		{
-		}
-
-		public bool Search(string[] query)
-		{
-			return SearchUtility.Matches(this.Name, query);
-		}
+		this.Tags.Add("Named");
 	}
 
-	public class CurrentActorsLibraryProvider : SourceBase
+	public ActorBackupAppearance(Actor actor)
+		: base(null)
 	{
-		public ILogger Log { get; init; } = Logging.ForContext<CurrentActorsLibraryProvider>();
-		public override string Name => "Current Actors";
+		this.name = actor.Name;
+		this.DrawData = actor.DrawData;
+		this.ModelId = actor.ModelCharaRowId;
 
-		public void OnEnterGroupPose()
+		this.Tags.Add("Named");
+	}
+
+	public DrawDataContainer DrawData { get; private set; }
+	public uint ModelId { get; private set; }
+	public override string Name => this.name ?? string.Empty;
+
+	public unsafe void Apply(Actor* actor)
+	{
+		this.Apply(actor, Actor.UpdateSource.Library);
+	}
+
+	public unsafe void Apply(Actor* actor, Actor.UpdateSource source)
+	{
+		bool redraw = this.ModelId != actor->ModelCharaRowId;
+
+		actor->UpdateModel(this.ModelId, source, false);
+		actor->UpdateCustomize(this.DrawData.CustomizeData, redraw, source);
+		actor->UpdateEquipment(this.DrawData.EquipmentModelIds, source);
+	}
+
+	protected override string GetInternalId() => this.Name;
+}
+
+public class GroupPoseCharactersLibrarySource : SourceBase
+{
+	public ILogger Log { get; init; } = Logging.ForContext<GroupPoseCharactersLibrarySource>();
+	public override string Name => Resources.Find("LOC_Library_GroupPoseCharactersLibrarySource", "GPose Actors");
+
+	public void OnEnterGroupPose()
+	{
+		this.Clear();
+
+		Task.Run(async () =>
 		{
-			this.Clear();
+			await Task.Delay(1500);
+			await Threads.RunOnFrameworkThread(() => this.BackupAll());
+		});
+	}
 
-			Task.Run(async () =>
-			{
-				await Task.Delay(1500);
-				await Threads.RunOnFrameworkThread(() => this.BackupAll());
-			});
-		}
+	public override void Scan()
+	{
+	}
 
-		public override void Scan()
+	protected override string GetInternalId() => "CurrentActorsLibraryProvider";
+
+	private unsafe void BackupAll()
+	{
+		// back up the appearance of every actor in gpose
+		for (int i = GroupPoseService.GPoseFirstActor; i < GroupPoseService.GPoseFirstActor + GroupPoseService.GPoseActorCount; ++i)
 		{
-		}
+			IntPtr? address = DalamudServices.ObjectTable?.GetObjectAddress(i);
+			if (address == null || address == IntPtr.Zero)
+				continue;
 
-		protected override string GetInternalId() => "CurrentActorsLibraryProvider";
+			Actor* actor = (Actor*)address;
 
-		private unsafe void BackupAll()
-		{
-			// back up the appearance of every actor in gpose
-			for (int i = GroupPoseService.GPoseFirstActor; i < GroupPoseService.GPoseFirstActor + GroupPoseService.GPoseActorCount; ++i)
-			{
-				IntPtr? address = DalamudServices.ObjectTable?.GetObjectAddress(i);
-				if (address == null || address == IntPtr.Zero)
-					continue;
-
-				Actor* actor = (Actor*)address;
-
-				Appearance appearance = new(actor);
-				this.Add(appearance);
-			}
+			ActorBackupAppearance appearance = new(actor);
+			this.Add(appearance);
 		}
 	}
 }
