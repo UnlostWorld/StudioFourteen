74efdf4c4aff0aef987e9faacda09728baa1ca6d
Yuki <yuuki.walsh@outlook.com>
2024-07-30 20:20:07 +1000

    added test app

---
+++ Test/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace ScreenshotStudio.Test;
+
+using ScreenshotStudio;
+using System.Windows;
+
+
+public partial class App : Application
+{
+	private readonly ServiceManager services = new();
+
+	protected override void OnStartup(StartupEventArgs e)
+	{
+		base.OnStartup(e);
+		Task.Run(this.services.Start);
+	}
+}
+
