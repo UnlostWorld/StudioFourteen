a48d8133e24495a990147a660e2a3aa02f949c20
Yuki <yuuki.walsh@outlook.com>
2024-08-07 21:29:07 +1000

    save window progress

--- ScreenshotStudio/Studio/SaveWindow.xaml.cs
+++ ScreenshotStudio/Studio/SaveWindow.xaml.cs
@@ -18,6 +18,24 @@ public partial class SaveWindow : PanelWindow
 	[AutoNotify] public string SaveLabel { get; private set; } = "Save Scene";
 	[AutoNotify] public FastObservableCollection<CharacterSave> Characters { get; init; } = new();
 
+	[AutoNotify] public string CharactersText => string.Format(ScreenshotStudio.Resources.Find("LOC_Save_CharactersText", string.Empty), 0, this.Characters.Count);
+
+	[AutoNotify] public bool IncludeLocation { get; set; }
+	[AutoNotify] public bool IncludeWeather { get; set; }
+	[AutoNotify] public bool IncludeTimeOfDay { get; set; }
+
+	[AutoNotify]
+	public bool IncludeEnvironment
+	{
+		get => this.IncludeLocation || this.IncludeWeather || this.IncludeTimeOfDay;
+		set
+		{
+			this.IncludeLocation = value;
+			this.IncludeWeather = value;
+			this.IncludeTimeOfDay = value;
+		}
+	}
+
 	protected override void OnOpened()
 	{
 		base.OnOpened();
@@ -75,11 +93,11 @@ public class CharacterSave(string name)
 {
 	public string Name { get; init; } = name;
 
-	public string TooltipText => string.Format(Resources.Find("LOC_Save_IncludeCharacter", string.Empty), this.Name);
-	public string AppearanceTooltipText => string.Format(Resources.Find("LOC_Save_IncludeAppearance", string.Empty), this.Name);
-	public string PoseTooltipText => string.Format(Resources.Find("LOC_Save_IncludePose", string.Empty), this.Name);
-	public string ExportPoseTooltipText => string.Format(Resources.Find("LOC_Save_ExportPoseTooltip", string.Empty), this.Name);
-	public string ExportAppearanceTooltipText => string.Format(Resources.Find("LOC_Save_ExportAppearanceTooltip", string.Empty), this.Name);
+	public string ToolTipText => string.Format(Resources.Find("LOC_Save_IncludeCharacterToolTip", string.Empty), this.Name);
+	public string AppearanceToolTipText => string.Format(Resources.Find("LOC_Save_IncludeAppearanceToolTip", string.Empty), this.Name);
+	public string PoseToolTipText => string.Format(Resources.Find("LOC_Save_IncludePoseToolTip", string.Empty), this.Name);
+	public string ExportPoseToolTipText => string.Format(Resources.Find("LOC_Save_ExportPoseTooltip", string.Empty), this.Name);
+	public string ExportAppearanceToolTipText => string.Format(Resources.Find("LOC_Save_ExportAppearanceTooltip", string.Empty), this.Name);
 
 	[AutoNotify] public bool IncludeCharacter { get; set; }
 	[AutoNotify] public bool IncludeAppearance { get; set; }
