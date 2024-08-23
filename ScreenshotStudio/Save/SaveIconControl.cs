d3cc106619fcb0eec81ec5fcaa71c1a77db1c78b
Yuki <yuuki.walsh@outlook.com>
2024-08-24 02:46:12 +1000

    save animation

---
+++ ScreenshotStudio/Save/SaveIconControl.cs
@@ -0,0 +1,25 @@
+﻿namespace ScreenshotStudio.Save;
+
+using DependencyPropertyGenerator;
+using System.Windows;
+
+[RoutedEvent("Saved", RoutedEventStrategy.Direct)]
+public partial class SaveIconControl : View
+{
+	protected override void OnLoaded()
+	{
+		this.Services.Save.Saved += this.OnSaveSaved;
+		base.OnLoaded();
+	}
+
+	protected override void OnUnloaded()
+	{
+		this.Services.Save.Saved -= this.OnSaveSaved;
+		base.OnLoaded();
+	}
+
+	private void OnSaveSaved()
+	{
+		this.Dispatcher.BeginInvoke(this.OnSaved);
+	}
+}
