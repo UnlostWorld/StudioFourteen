8ee663a1f768742eed9e520eca1ea7698bca78e8
Yuki <yuuki.walsh@outlook.com>
2024-10-27 21:45:09 +1100

    orbit target camera lerp on target changes

--- StudioFourteen/Cameras/OrbitTargetCamera.cs
+++ StudioFourteen/Cameras/OrbitTargetCamera.cs
@@ -1,10 +1,20 @@
 ﻿namespace StudioFourteen.Cameras;
 
 using PropertyChanged.SourceGenerator;
+using System;
 using System.Numerics;
+using WpfUtils.Animation;
 
 public partial class OrbitTargetCamera : OrbitCamera
 {
+	public const float TargetBlendDuration = 0.250f;
+
+	private readonly EasingFunctionBase targetEase = new SineEase();
+	private int currentTargetIndex = -1;
+	private Vector3 oldTargetPosition;
+	private Vector3 currentTargetPosition;
+	private float targetBlend = -1;
+
 	[Notify] private Vector3 targetOffset = new(0, 0, 0);
 
 	public override string TypeName => Resources.Find("LOC_OrbitTargetCamera", "Orbit Target");
@@ -35,9 +45,28 @@ public partial class OrbitTargetCamera : OrbitCamera
 		this.TargetOffset = targetOffset;
 		this.desiredMove = Vector3.Zero;
 
+		if (this.currentTargetIndex != this.Services.Target.TargetObjectIndex && this.currentTargetIndex != -1)
+		{
+			this.oldTargetPosition = this.currentTargetPosition;
+			this.targetBlend = TargetBlendDuration;
+		}
+
 		if (this.Services.Target.HasValidTarget)
 		{
-			Vector3 targetPos = this.Services.Target.Target->Position;
+			this.currentTargetPosition = this.Services.Target.Target->Position;
+			this.currentTargetIndex = this.Services.Target.TargetObjectIndex;
+
+			Vector3 targetPos = this.currentTargetPosition;
+
+			if (this.targetBlend > 0)
+			{
+				this.targetBlend -= deltaTime;
+				float blendValue = this.targetBlend / TargetBlendDuration;
+				blendValue = Math.Clamp(blendValue, 0.0f, 1.0f);
+				blendValue = this.targetEase.Ease(blendValue, EasingFunctionBase.EasingModes.EaseInOut);
+				targetPos = Vector3.Lerp(targetPos, this.oldTargetPosition, blendValue);
+			}
+
 			targetPos += this.TargetOffset;
 			this.Target = targetPos;
 		}
