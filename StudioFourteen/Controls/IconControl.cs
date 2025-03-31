7894a8939a923d666704975a906d6b0d9c4c9392
Yuki <Yuki.Walsh@Outlook.com>
2025-03-31 21:09:41 +1100

    unify icon systems

--- StudioFourteen/Controls/IconControl.cs
+++ StudioFourteen/Controls/IconControl.cs
@@ -1,4 +1,4 @@
-﻿// .                    @@             _____ _______ _    _ _____ _____ ____
+// .                    @@             _____ _______ _    _ _____ _____ ____
 //          @       @@@@@             / ____|__   __| |  | |  __ \_   _/ __ \
 //         @@@  @@@@                 | (___    | |  | |  | | |  | || || |  | |
 //         @@@@@@@@@  @    @          \___ \   | |  | |  | | |  | || || |  | |
@@ -15,56 +15,16 @@
 
 namespace StudioFourteen.Controls;
 
-using System;
 using System.Windows.Controls;
+using System.Windows.Media;
 using DependencyPropertyGenerator;
-using FontAwesome.Sharp;
-using PropertyChanged.SourceGenerator;
 
-[DependencyProperty<string>("Icon")]
-public partial class IconControl : Control
+[DependencyProperty<object>("Icon")]
+[DependencyProperty<Brush>("Foreground")]
+public partial class IconControl : ContentPresenter
 {
-	[Notify] private object? content;
-
-	partial void OnIconChanged(string? newValue)
+	partial void OnIconChanged(object? newValue)
 	{
-		if (newValue == null)
-			return;
-
-		if (newValue.StartsWith("fa-"))
-		{
-			try
-			{
-				IconFont font = IconFont.Solid;
-				string iconName = newValue.Substring(3);
-				if (iconName.StartsWith("line-"))
-				{
-					font = IconFont.Regular;
-					iconName = iconName.Substring(5);
-				}
-
-				iconName = iconName.Replace("-", string.Empty);
-
-				FontAwesomeIcon icon = new();
-				icon.Icon = Enum.Parse<IconChar>(iconName, true);
-				icon.Font = font;
-				this.Content = icon;
-			}
-			catch (Exception)
-			{
-				Logging.Shared.Warning($"Font Awesome icon {newValue} not found");
-				this.Content = IconChar.Question;
-			}
-		}
-		else
-		{
-			this.Content = $"pack://application:,,,/StudioFourteen;component/Assets/Icons/{newValue}.svg";
-		}
+		this.Content = newValue;
 	}
-}
-
-public class FontAwesomeIcon
-{
-	public IconChar Icon { get; set; }
-	public IconFont Font { get; set; } = IconFont.Solid;
 }
