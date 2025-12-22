cbd0ca767c875b22f936c15229ae8f5037f2ef4c
Yuki <Yuki.Walsh@Outlook.com>
2025-12-22 22:36:50 +1100

    fix compile

--- StudioFourteen/GameData/Sheets/CharaMakeType.cs
+++ StudioFourteen/GameData/Sheets/CharaMakeType.cs
@@ -35,10 +35,10 @@ public readonly unsafe struct CharaMakeType(ExcelPage page, uint offset, uint ro
 
 	static CharaMakeType IExcelRow<CharaMakeType>.Create(ExcelPage page, uint offset, uint row) => new(page, offset, row);
 
-	private static CharaMakeStructStruct CharaMakeStructCtor(ExcelPage page, uint parentOffset, uint offset, uint i) => new(page, parentOffset, offset + i * 452);
+	private static CharaMakeMenu CharaMakeStructCtor(ExcelPage page, uint parentOffset, uint offset, uint i) => new(page, parentOffset, offset + (i * 452));
 	private static byte VoiceStructCtor(ExcelPage page, uint parentOffset, uint offset, uint i) => page.ReadUInt8(offset + 12656 + i);
-	private static FacialFeatureOptionStruct FacialFeatureOptionCtor(ExcelPage page, uint parentOffset, uint offset, uint i) => new(page, parentOffset, offset + 12668 + i * 28);
-	private static EquipmentStruct EquipmentCtor(ExcelPage page, uint parentOffset, uint offset, uint i) => new(page, parentOffset, offset + 12896 + i * 56);
+	private static FacialFeatureOptionStruct FacialFeatureOptionCtor(ExcelPage page, uint parentOffset, uint offset, uint i) => new(page, parentOffset, offset + 12668 + (i * 28));
+	private static EquipmentStruct EquipmentCtor(ExcelPage page, uint parentOffset, uint offset, uint i) => new(page, parentOffset, offset + 12896 + (i * 56));
 
 	public readonly struct CharaMakeMenu(ExcelPage page, uint parentOffset, uint offset)
 	{
